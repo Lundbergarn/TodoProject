5a9f178367d888616d7afed7373210fc5e867715
Christoffer Lundberg <lundbergarn2@hotmail.com>
2019-10-25 16:32:46 +0200

    Updates

---
+++ precache-manifest.3619b8d0ed3a82cae3269b7b52ee92bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fe679fc2867ced658534a925fdeeee1",
+    "url": "/TodoProject/index.html"
+  },
+  {
+    "revision": "3361f0ffef370f22a2ae",
+    "url": "/TodoProject/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "6021c09bb574ccab5b40",
+    "url": "/TodoProject/static/css/main.323fea3d.chunk.css"
+  },
+  {
+    "revision": "3361f0ffef370f22a2ae",
+    "url": "/TodoProject/static/js/2.f5b3fcc1.chunk.js"
+  },
+  {
+    "revision": "6021c09bb574ccab5b40",
+    "url": "/TodoProject/static/js/main.dc5800d2.chunk.js"
+  },
+  {
+    "revision": "4ca2bebf9781f188ebe0",
+    "url": "/TodoProject/static/js/runtime-main.0207cffa.js"
+  }
+]);
